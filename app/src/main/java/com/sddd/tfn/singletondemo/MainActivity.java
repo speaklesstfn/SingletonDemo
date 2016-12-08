3d9e143cf8693c45809d8c624482da46ac54e23d
speaklesstfn <xiaddtfn@126.com>
2016-12-08 10:47:35 +0800

    commit test

--- app/src/main/java/com/sddd/tfn/singletondemo/MainActivity.java
+++ app/src/main/java/com/sddd/tfn/singletondemo/MainActivity.java
@@ -12,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
+
+        
         new Thread(new Runnable() {
             @Override
             public void run() {
